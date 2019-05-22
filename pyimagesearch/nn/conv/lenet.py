8f68e82281882cf91a566826b776d275bbdbfad5
wall1350 <wall1350@gmail.com>
2019-05-22 21:13:20 +0800

    First version
    the assignments

---
+++ pyimagesearch/nn/conv/lenet.py
@@ -0,0 +1,47 @@
+from keras.models import Sequential
+from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Flatten
+from keras.layers.core import Dense
+from keras import backend as K
+
+class LeNet:
+    @staticmethod
+    def build(width, height, depth, classes):
+        # initialize the model
+        model = Sequential()
+        inputShape = (height , width ,depth)
+        
+        if K.image_data_format()=="channels_first":
+            inputShape = (height , width ,depth)
+        # first set of CONV => RELU => POOL
+        model.add(Convolution2D(20, (5, 5), padding="same",
+            input_shape=inputShape))
+        model.add(Activation("relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+        # second set of CONV => RELU => POOL
+        model.add(Convolution2D(50, (5, 5), padding="same"))
+        model.add(Activation("relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        
+         # second set of CONV => RELU => POOL
+        model.add(Convolution2D(50, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        
+        # set of FC => RELU layers
+        model.add(Flatten())
+        model.add(Dense(500))
+        model.add(Activation("relu"))
+
+        # softmax classifier
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
+
+        # if weightsPath is specified load the weights
+        #if weightsPath is not None:
+            #model.load_weights(weightsPath)
+
+        return model
